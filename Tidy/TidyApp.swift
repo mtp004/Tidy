328081c880b562e51a273d465e99539b85ba4fb1
Tri Pham <tri.pham121@icloud.com>
2025-03-27 20:55:34 -0400

    Initial Commit

---
+++ Tidy/TidyApp.swift
@@ -0,0 +1,20 @@
+//
+//  TidyApp.swift
+//  Tidy
+//
+//  Created by Tri Pham on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TidyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ TidyTests/TidyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TidyTests.swift
+//  TidyTests
+//
+//  Created by Tri Pham on 3/27/25.
+//
+
+import Testing
+@testable import Tidy
+
+struct TidyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
